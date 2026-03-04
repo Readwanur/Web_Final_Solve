9830a485b51c6594cf4f0d4813f1e13b1893add7
Readwanur <melodygangster@gmail.com>
2026-03-05 00:00:12 +0600

    update

--- Solves/Web_Final_243_Ques_1/script.js
+++ Solves/Web_Final_243_Ques_1/script.js
@@ -1,19 +1,36 @@
+let attempt = 0;
 
+const randomNumber = Math.floor(Math.random() * (5000 - 500 + 1)) + 500;
 
 function guessTheNumber() {
-  const input = document.getElementById("input").value;
+  const guess = parseInt(document.getElementById("input").value);
+  if(isNaN(guess)) return;
+  attempt++;
 
-  const randomNumber = parseFloat(input) % 500;
+  // document.getElementById("feedBack").innerHTML = randomNumber;
 
-  document.getElementById("feedBack").innerHTML = randomNumber;
+  if (attempt <= 5) {
+    if (guess > randomNumber) {
+      document.getElementById("feedBack").innerHTML = "Too high!";
+      document.getElementById("input").value = "";
+    } else if (guess < randomNumber) {
+      document.getElementById("feedBack").innerHTML = "Too low!";
+      document.getElementById("input").value = "";
+    } else {
+      document.getElementById("feedBack").innerHTML = "Correct!";
+      document.getElementById("input").disabled = true;
+      document.getElementById("input").value = "";
+      return;
+    }
+  } else if (attempt > 5) {
+    document.getElementById("feedBack").innerHTML =
+      "Out of guesses!" + "<br>" + "The secret number is " + randomNumber;
+    document.getElementById("input").disabled = true;
+    document.getElementById("input").value = "";
+    attempt = 0;
+    return;
+  }
 
   //Clearing the input field
   document.getElementById("input").value = "";
-  
-}
-
-//Optional
-function resetFeedBack() {
-  //Clearing the feedBack label
-  document.getElementById("feedBack").innerHTML = "";
 }
